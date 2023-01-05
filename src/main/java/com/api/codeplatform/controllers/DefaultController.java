b38b1d741c800000ff7fdb7d3d9e151701c6431d
Student42069 <lehackdeshackers@gmail.com>
2023-01-05 00:53:00 -0500

    Handle error pages

---
+++ src/main/java/com/api/codeplatform/controllers/DefaultController.java
@@ -0,0 +1,23 @@
+package com.api.codeplatform.controllers;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class DefaultController implements ErrorController {
+
+    @RequestMapping("/")
+    public String defaultMapping(HttpServletResponse httpResponse){
+        return "redirect:code/new";
+    }
+
+    @RequestMapping("/error")
+    public RedirectView handleError() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("https://github.com/Student42069/Code-Sharing-Platform#readme");
+        return redirectView;
+    }
+}
